9932230f2372fff0a17aa2377b7ffd9b861ccb28
Fovir <fovir@disroot.org>
2024-08-10 12:49:15 +0800

    add firstref.cpp

---
+++ Chapter8/8.2-firstref/firstref.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	int rats = 101;
+	int & rodents = rats;
+
+	cout << "rates = " << rats
+		<< ", rodents = " << rodents << endl;
+
+	rodents++;
+	cout << "rats = " << rats
+		<< ", rodents = " << rodents << endl;
+
+	cout << "rats address = " << &rats
+		<< ", rodents address = " << &rodents << endl;
+
+	return 0;
+}
