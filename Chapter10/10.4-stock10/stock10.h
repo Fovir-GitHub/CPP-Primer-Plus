d3715a3dd470636c56aa90c364795cf8e5d43b61
Fovir <fovir@disroot.org>
2024-08-15 20:59:10 +0800

    add usestock1

---
+++ Chapter10/10.4-stock10/stock10.h
@@ -0,0 +1,27 @@
+#ifndef _STOCK10_H_
+#define _STOCK10_H_
+
+#include<string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+
+	void set_tot() { total_val = shares * share_val; }
+
+public:
+	Stock();
+	Stock(const std::string & co, long n = 10, double pr = 0.0);
+	~Stock();
+
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+};
+
+#endif // !_STOCK10_H_
