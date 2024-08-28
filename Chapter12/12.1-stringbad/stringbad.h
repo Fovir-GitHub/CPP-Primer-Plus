8446c1b1392118389c6e1c6c3a7b6433e81c56fb
Fovir <fovir@disroot.org>
2024-08-28 17:08:04 +0800

    add badstring

---
+++ Chapter12/12.1-stringbad/stringbad.h
@@ -0,0 +1,22 @@
+#ifndef _STRINGBAD_H_
+#define _STRINGBAD_H_
+
+#include<iostream>
+
+class StringBad
+{
+private:
+	char * str;
+	int len;
+	static int num_strings;
+
+public:
+	StringBad(const char * s);
+	StringBad();
+	~StringBad();
+
+	friend std::ostream & operator<<(std::ostream & os,
+		const StringBad & st);
+};
+
+#endif // !_STRINGBAD_H_
