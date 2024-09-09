e89f22c1e1da7d050b2f30d95fbbc71e8473fe08
Fovir <fovir@disroot.org>
2024-09-09 22:34:18 +0800

    add queuetp and show how nested class work

---
+++ Chapter15/15.5-queuetp/queuetp.h
@@ -0,0 +1,90 @@
+#ifndef _QUEUETP_H_
+#define _QUEUETP_H_
+
+template <class Item>
+class QueueTP
+{
+private:
+    enum { Q_SIZE = 10 };
+
+    class Node
+    {
+    public:
+        Item item;
+        Node * next;
+        Node(const Item & i) :item(i), next(nullptr) {}
+    };
+    Node * front;
+    Node * rear;
+    int items;
+    const int qsize;
+
+    QueueTP(const QueueTP & q) :qsize(0) {}
+    QueueTP & operator=(const QueueTP & q) { return *this; }
+
+public:
+    QueueTP(int qs = Q_SIZE);
+    ~QueueTP();
+
+    bool isempty()const { return items == 0; }
+    bool isfull()const { return items == qsize; }
+    int queuecount()const { return items; }
+    bool enqueue(const Item & item);
+    bool dequeue(Item & item);
+};
+
+template<class Item>
+QueueTP<Item>::QueueTP(int qs) :qsize(qs)
+{
+    front = rear = nullptr;
+    items = 0;
+}
+
+template<class Item>
+QueueTP<Item>::~QueueTP()
+{
+    Node * temp;
+    while (front)
+    {
+        temp = front;
+        front = front->next;
+        delete temp;
+    }
+}
+
+template<class Item>
+bool QueueTP<Item>::enqueue(const Item & item)
+{
+    if (isfull())
+        return false;
+
+    Node * add = new Node(item);
+    items++;
+    if (front == nullptr)
+        front = add;
+    else
+        rear->next = add;
+    rear = add;
+
+    return true;
+}
+
+template<class Item>
+bool QueueTP<Item>::dequeue(Item & item)
+{
+    if (front == nullptr)
+        return false;
+    item = front->item;
+    items--;
+
+    Node * temp = front;
+    front = front->next;
+    delete temp;
+
+    if (items == 0)
+        rear = nullptr;
+
+    return true;
+}
+
+#endif // !_QUEUETP_H_
