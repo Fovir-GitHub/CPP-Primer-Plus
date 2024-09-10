be64d33015f37149059fc28aa8917148c22fc120
Fovir <fovir@disroot.org>
2024-09-10 14:55:48 +0800

    add error3.cpp show how try and catch work

---
+++ Chapter15/15.9-error3/error3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+double hmean(double a, double b);
+
+int main(void)
+{
+    double x, y, z;
+
+    std::cout << "Enter two numbers: ";
+    while (std::cin >> x >> y)
+    {
+        try
+        {
+            z = hmean(x, y);
+        }
+        catch (const char * s)
+        {
+            std::cout << s << std::endl;
+            std::cout << "Enter a new pair of numbers: ";
+            continue;
+        }
+
+        std::cout << "Harmonic mean of " << x << " and "
+            << y << " is " << z << '\n';
+        std::cout << "Enter next set of numbers <q to quit>: ";
+    }
+    std::cout << "Bye!\n";
+
+    return 0;
+}
+
+double hmean(double a, double b)
+{
+    if (a == -b)
+        throw "bad hmean() arguments: a = -b not allowed";
+    return (double) 2 * a * b / (a + b);
+}
